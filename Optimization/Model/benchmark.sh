6b7a1bf9091fcabb3b29ed29cdb96e5d0bf9253a
Ching-En <nm6114083@gs.ncku.edu.tw>
2022-08-30 01:51:50 +0800

    add openMP for parallelisation

--- Optimization/Model/benchmark.sh
+++ Optimization/Model/benchmark.sh
@@ -5,5 +5,6 @@ mkdir -p log
 for i in $@
 do
     ./bin/eng_oral_opt -seed 1 -epsilon 0.05 -iteration $i 2>> log/opt.txt
+    ./bin/eng_oral_openmp -seed 1 -epsilon 0.05 -iteration $i 2>> log/openmp.txt
     ./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
 done
