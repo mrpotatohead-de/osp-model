409e5d8f4b54033f92028900594b3412b92d5694
ChingEn Kuo <nm6114083@gs.ncku.edu.tw>
2022-10-12 09:54:13 +0800

    add blis option

--- Optimization/Model/benchmark.sh
+++ Optimization/Model/benchmark.sh
@@ -4,7 +4,7 @@ mkdir -p log
 
 for i in $@
 do
-    ./bin/eng_oral_opt -seed 1 -epsilon 0.05 -iteration $i 2>> log/opt.txt
-    ./bin/eng_oral_openmp -seed 1 -epsilon 0.05 -iteration $i 2>> log/openmp.txt
+    #./bin/eng_oral_blis -seed 1 -epsilon 0.05 -iteration $i 2>> log/opt.txt
+    #./bin/eng_oral_openmp -seed 1 -epsilon 0.05 -iteration $i 2>> log/openmp.txt
     ./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
 done
