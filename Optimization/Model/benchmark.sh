aa4555e60ad77ec40880080d1cd801f89156d2bc
ChingEn Kuo <nm6114083@gs.ncku.edu.tw>
2022-10-19 23:01:33 +0800

    add Makefile blis option in Sim3

--- Optimization/Model/benchmark.sh
+++ Optimization/Model/benchmark.sh
@@ -4,7 +4,9 @@ mkdir -p log
 
 for i in $@
 do
-    #./bin/eng_oral_blis -seed 1 -epsilon 0.05 -iteration $i 2>> log/opt.txt
+    echo "blis"
+    ./bin/eng_oral_blis -seed 1 -epsilon 0.05 -iteration $i 2>> log/opt.txt
     #./bin/eng_oral_openmp -seed 1 -epsilon 0.05 -iteration $i 2>> log/openmp.txt
+    echo "raw"
     ./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
 done
