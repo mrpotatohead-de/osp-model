d9e111584263e71a605e431a7982b5962c1f0b3d
DavidKuo <nm6114083@gs.ncku.edu.tw>
2023-01-15 16:25:56 +0800

    add cublas option

--- Optimization/Model/benchmark.sh
+++ Optimization/Model/benchmark.sh
@@ -4,9 +4,9 @@ mkdir -p log
 
 for i in $@
 do
-    echo "blis"
-    ./bin/eng_oral_blis -seed 1 -epsilon 0.05 -iteration $i 2>> log/opt.txt
+    echo "cuda"
+    ./bin/eng_oral_cuda -seed 1 -epsilon 0.05 -iteration $i #2>> log/opt.txt
     #./bin/eng_oral_openmp -seed 1 -epsilon 0.05 -iteration $i 2>> log/openmp.txt
-    echo "raw"
-    ./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
+    #echo "raw"
+    #./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
 done
