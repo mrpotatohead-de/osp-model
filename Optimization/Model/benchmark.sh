f1b1ffa1ad365f063ac694e917ef3408fa9a45ec
DavidKuo <nm6114083@gs.ncku.edu.tw>
2023-01-15 16:30:42 +0800

    benchmark for raw and cublas

--- Optimization/Model/benchmark.sh
+++ Optimization/Model/benchmark.sh
@@ -8,5 +8,5 @@ do
     ./bin/eng_oral_cuda -seed 1 -epsilon 0.05 -iteration $i #2>> log/opt.txt
     #./bin/eng_oral_openmp -seed 1 -epsilon 0.05 -iteration $i 2>> log/openmp.txt
     #echo "raw"
-    #./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
+    ./bin/eng_oral_raw -seed 1 -epsilon 0.05 -iteration $i 2>> log/raw.txt
 done
