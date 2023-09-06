ceb2980244c3bb1b84be31d4845f99227e3e9275
DavidKuo <nm6114083@gs.ncku.edu.tw>
2023-09-07 02:13:02 +0800

    modified some paths

--- Simulation3/Model/eng_oral.sh
+++ Simulation3/Model/eng_oral.sh
@@ -9,7 +9,7 @@ do
      
      echo "version=${i}, iteration=${k}"
 
-     ./bin/eng_oral -seed ${i} -epsilon 0.05 -iteration ${k}
+     /home/nm6114083/my_ws/osp-model/Simulation3/Model/bin/eng_oral_blis -seed ${i} -epsilon 0.05 -iteration ${k}
      
 	done
 done
